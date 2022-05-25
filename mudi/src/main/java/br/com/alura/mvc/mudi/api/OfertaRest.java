a2e0d424c05ed593c52e0c9c59b70b7025a073d0
srssilva <ris.simone@gmail.com>
2022-05-25 17:08:12 -0300

    Correção

--- mudi/src/main/java/br/com/alura/mvc/mudi/api/OfertaRest.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/api/OfertaRest.java
@@ -1,47 +1,5 @@
 package br.com.alura.mvc.mudi.api;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.alura.mvc.mudi.dto.RequisicaoNovaOferta;
-import br.com.alura.mvc.mudi.model.Oferta;
-import br.com.alura.mvc.mudi.model.Pedido;
-import br.com.alura.mvc.mudi.repository.PedidoRepository;
-
-@RestController
-@RequestMapping("/api/oferta")
-public class OfertaRest {
-
-	@Autowired
-	private PedidoRepository pedidoRepository; 
-
-	@PostMapping("/oferta")
-	public Oferta criaOfertas(RequisicaoNovaOferta requisicao) {
-		
-		Optional<Pedido> idPedidoRequisicao = pedidoRepository.findById(requisicao.pedidoId);
-		
-		if(!idPedidoRequisicao.isPresent()) {
-			return null;
-		}
-		
-		Pedido pedido = idPedidoRequisicao.get();		
-		Oferta nova = requisicao.toOferta();
-		
-		nova.setPedido(pedido);
-		pedido.getOfertas().add(nova);
-		
-		pedidoRepository.save(pedido);
-				
-		return nova;
-	}
-}
-=======
-package br.com.alura.mvc.mudi.api;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
